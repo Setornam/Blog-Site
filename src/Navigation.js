59f32cd79975292f1f1d94224efcc484466af031
Setor Kwame <setorkg@gmail.com>
2024-01-30 17:33:42 +0000

    responsiveness

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,19 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const Navigation = () => {
+  const [isMenuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!isMenuOpen);
+  };
+
   return (
     <div>
-      <ul>
-      <li>
-          <Link to="/">Home</Link>
+      <div className={`mobile-menu-icon ${isMenuOpen ? 'open' : ''}`} onClick={toggleMenu}>
+        <div className="bar"></div>
+        <div className="bar"></div>
+        <div className="bar"></div>
+      </div>
+
+      <ul className={`nav-list ${isMenuOpen ? 'open' : ''}`}>
+        <li>
+          <Link to="/" onClick={() => setMenuOpen(false)}>Home</Link>
         </li>
         <li>
-          <Link to="/blog">New Blog</Link>
+          <Link to="/blog" onClick={() => setMenuOpen(false)}>New Blog</Link>
         </li>
       </ul>
     </div>
   );
-}
+};
 
 export default Navigation;
